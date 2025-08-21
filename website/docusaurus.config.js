69a343d4a63909e1be9766687d997442344a0e6a
Hyunsung Shin <2005hyunsung@gmail.com>
2025-08-21 16:32:01 +0800

    try fixing deployment 2

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -20,15 +20,15 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://gust10.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/Handible-main/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'gust10', // Usually your GitHub org/user name.
-  projectName: 'Handible', // Usually your repo name.
+  projectName: 'Handible-main', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
